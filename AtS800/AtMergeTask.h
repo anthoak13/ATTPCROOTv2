36712e346db9170a03306650d1c08bc0bcf3aec0
Simon <giraud@frib.msu.edu>
2024-06-04 14:07:14 -0400

    d,2He - added plots to help for the offline merging, inverse map updates, brho for simulation

--- AtS800/AtMergeTask.h
+++ AtS800/AtMergeTask.h
@@ -6,6 +6,9 @@
 #include <Rtypes.h>
 #include <TString.h>
 
+#include <TCanvas.h>
+#include <TH1D.h>
+
 #include "S800Ana.h"
 
 #include <vector>
@@ -40,6 +43,9 @@ public:
    void SetMTDCXfRange(std::vector<Double_t> vec);
    void SetATTPCClock(Bool_t value = kTRUE);
    void SetATTPCClockFreq(Double_t value);
+   void ShowTSDiagnostic(Bool_t value = kFALSE);
+
+   void diagnosticPlotStyle(Int_t plotId);
 
    Int_t GetS800TsSize();
    Int_t GetMergedTsSize();
@@ -68,9 +74,15 @@ private:
    std::vector<Double_t> fMTDCObjRange;
    std::vector<Double_t> fMTDCXfRange;
 
+   // std::vector<Long64_t> fATTPCTs;
+   // std::vector<Double_t> fATTPCEvt;
+
    TF1 *fS800TsFunc{};
    Double_t fGlom{2};
    Double_t fATTPCClockFreq{};
+
+   TFile *diagFile;
+   TCanvas *diagCanvas;
 
    std::vector<TString> fcutPID1File;
    std::vector<TString> fcutPID2File;
@@ -78,10 +90,17 @@ private:
 
    Bool_t fIsPersistence{false};
    Bool_t fUseATTPCClock{false};
+   Bool_t fShowTSDiagnostic{false};
 
    S800Ana fS800Ana;
 
    TGraph *fS800TsGraph{};
+   TGraph *fATTPCTsGraph = nullptr;
+   TGraph *fATTPCandS800TsGraph = nullptr;
+   TGraph *fdiffTsGraph = nullptr;
+   TH1D   *fdiffTsHist = nullptr;
+
+   std::vector<TString> plotNames; 
 
    ClassDef(AtMergeTask, 1);
 };
